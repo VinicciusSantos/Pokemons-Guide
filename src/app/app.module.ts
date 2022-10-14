a9985fbf60f99988dd5bc5e1dfcbc704cb5bb649
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-10-14 11:07:01 -0300

    :lipstick: feat: Configurando temas e componente para troca

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FavoritosComponent } from './pages/favoritos/favoritos.component';
+import { ToggleThemeComponent } from './components/toggle-theme/toggle-theme.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    FavoritosComponent
+    FavoritosComponent,
+    ToggleThemeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/toggle-theme/toggle-theme.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ThemeService } from 'src/app/services/theme.service';
+
+@Component({
+  selector: 'app-toggle-theme',
+  templateUrl: './toggle-theme.component.html',
+  styleUrls: ['./toggle-theme.component.scss']
+})
+export class ToggleThemeComponent implements OnInit {
+  isDarkMode: boolean;
+
+  constructor(private themeService: ThemeService) {
+    this.themeService.initTheme();
+    this.isDarkMode = this.themeService.isDarkMode();
+  }
+
+  ngOnInit(): void {
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = this.themeService.isDarkMode();
+
+    this.isDarkMode
+      ? this.themeService.update('light-mode')
+      : this.themeService.update('dark-mode');
+  }
+}
